81da71c123dc12ba5841613201f905bce0743451
Craig <CMPeoples3@gmail.com>
2024-11-29 09:52:37 -0500

    Virutalization process

--- virtualization/process/process.go
+++ virtualization/process/process.go
@@ -1,5 +1,12 @@
 package process
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type STATE string
 
 // Process states following the xv6 Proc Structure
@@ -8,7 +15,7 @@ const (
 	EMBRYO   STATE = "EMBRYO"
 	SLEEPING STATE = "SLEEPING"
 	BLOCKED  STATE = "BLOCKED"
-	RUNNABLE STATE = "RUNNABLE" // may rename to PENDING
+	RUNNABLE STATE = "RUNNABLE"
 	ZOMBIE   STATE = "ZOMBIE"
 	RUNNING  STATE = "RUNNING"
 )
@@ -25,22 +32,67 @@ type Context struct {
 	ebp int
 }
 
+func getNextPID() uuid.UUID {
+	return uuid.New()
+}
+
 // Process structure following the xv6 Proc Structure
 type Process struct {
-	PID    int
+	PID    uuid.UUID
 	State  STATE
 	Parent *Process
-	killed int
+	Child  *Process
+	// killed int // ?
 	// chan -- not sure?
 	// file File
 	// cwd *inode -- hmm may not need for my impl
-	context Context // context switch criteria
-	Size    uint    // size of process memory -- may not need
+	// context Context // context switch criteria
+	Size uint // size of process memory -- may not need
 	// trapFrame Trap -- Trap for the current interupt
-	AddressSpace []byte // Thoguht we needed?
-	instructions []byte // thought we needed?
+	AddressSpace []byte // Memory space allotted for the process...
+	instructions []byte // For now... This may be a func that just executes a timer...
 }
 
-func (p *Process) Fork()
-func (p *Process) Wait()
-func (p *Process) Exec()
+// Fork spawns a new process from the current process' state ... Almost feel liek fork should be higher level, not on a process
+func (p *Process) fork() *Process {
+	child := &Process{
+		PID:          getNextPID(), // Function to generate unique PIDs
+		State:        EMBRYO,
+		Parent:       p,
+		AddressSpace: append([]byte{}, p.AddressSpace...), // Copy memory
+		instructions: append([]byte{}, p.instructions...), // Copy instructions
+	}
+	child.State = RUNNABLE
+	return child
+}
+
+// Wait is called by a parent process to wait until a child process completes.
+func (p *Process) Wait() {
+	if p.Child == nil {
+		return
+	}
+
+	// Only works if child is spun up in separate routine...
+	for p.Child.State != ZOMBIE {
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	p.Parent.RemoveChild()
+}
+
+// func (p *Process) Exec()
+
+func (p *Process) RemoveChild() {
+	p.Child = nil
+}
+
+func (p *Process) CreateChild() error {
+	if p.Child != nil {
+		return fmt.Errorf("process already has child process")
+	}
+
+	child := p.fork()
+	p.Child = child
+
+	return nil
+}
